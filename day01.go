cd2ddcc865a85036e90dc2b47758af62ec84fb99
sansuiqiang <1141192431@qq.com>
2025-03-03 14:02:28 +0800

    Changes

--- day01.go
+++ day01.go
@@ -7,6 +7,7 @@ func main() {
 	fmt.Println("hello , golang")
 	fmt.Println("hello , golang")
 	fmt.Println("hello , golang")
+	fmt.Println("hello , golang")
 	fmt.Println(a, b, c)
 	//输出hello , golang
 	//第一次见到：= 用于声明并初始化变量
